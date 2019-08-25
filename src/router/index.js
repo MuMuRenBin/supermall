5df0298b64e1a2446788cbff586df83bd700a1d0
rbb <940774475@qq.com>
2019-08-25 21:15:03 +0800

    20190825更新

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {path:'',redirect:'/home'},
+    {path:'/home',component:()=>import('../views/home/Home.vue')},
+    {path:'/category',component:()=>import('../views/category/Category.vue')},
+    {path:'/cart',component:()=>import('../views/cart/Cart.vue')},
+    {path:'/profile',component:()=>import('../views/profile/Profile.vue')},
+  ]
+})
---
+++ src/views/cart/Cart.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        333
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Cart',
+
+}
+</script>
+
+<style scoped>
+
+</style>
